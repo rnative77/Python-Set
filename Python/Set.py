0c0ef0328cfa7c01cb660e9511ad9b904720f0e0
Aldo <wanderson.aldo@gmail.com>
2026-04-11 09:31:09 -0300

    Python Set

---
+++ Python/Set.py
@@ -0,0 +1,29 @@
+# set tasks
+myset = {"apple", "banana", "cherry"}
+print(myset)
+
+myset = {"apple", "banana", "cherry", "apple"}
+print(myset)
+
+myset = {"apple", "banana", "cherry", True, 1, 2}
+print(myset)
+
+myset = {"apple", "banana", "cherry", False, True, 0}
+print(myset)
+
+myset = {"apple", "banana", "cherry"}
+print(len(myset))
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+print(set3)
+
+set1 = {"abc", 34, True, 40, "male"}
+print(set1)
+
+myset = {"apple", "banana", "cherry"}
+print(type(myset))
+
+myset = set(("apple", "banana", "cherry"))
+print(myset)
